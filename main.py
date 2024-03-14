b1a039e9b55ce88f0eaafe4dc07d828a3e03a8b4
Elkarapuzik <shmakov-10@bk.ru>
2024-03-14 18:58:21 +0300

    Добавили отправку поста в тгк

--- main.py
+++ main.py
@@ -5,10 +5,16 @@ from pprint import pprint
 import requests
 from termcolor import cprint
 from bs4 import BeautifulSoup
+import telegram
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
 #______________________________
 
-def get_coopland_info():
+coopland_link = 'https://coop-land.ru'
+
+def get_coopland_info(coopland_link_base):
 
     response = requests.get(f"https://coop-land.ru/helpguides/new/")
     response.raise_for_status()
@@ -21,15 +27,32 @@ def get_coopland_info():
 
     last_news_block = soup.find('div', attrs={"class": "article clr"})
 
-    last_news_picture_url = 'https://coop-land.ru' + last_news_block.find('img')['data-src']
+    last_news_picture_url = coopland_link_base + last_news_block.find('img')['data-src']
     
     last_news_source_link = last_news_block.find('a')['href']
     
     return last_news_headline, last_news_description, last_news_picture_url, last_news_source_link
 
-coopland_headline, coopland_descriotipn, coopland_picture_url, coopland_source_link = get_coopland_info()
 
-cprint(coopland_descriotipn, 'green')
-cprint(coopland_headline, 'green')
-cprint(coopland_source_link, 'yellow')
-cprint(coopland_picture_url , 'yellow')
+
+coopland_headline, coopland_descriotipn, coopland_picture_url, coopland_source_link = get_coopland_info(coopland_link)
+
+post = (f"""\
+{coopland_headline} 
+{coopland_descriotipn}
+[▶▶▶Источник]({coopland_source_link})
+""")
+
+#_______________________________________________________________
+telegram_bot_token = os.getenv("TELEGRAM_BOT_TOKEN")
+telegram_chat_id = os.getenv("TELEGRAM_CHAT_ID")
+
+
+bot = telegram.Bot(token=telegram_bot_token)
+
+bot.send_photo(chat_id=telegram_chat_id, photo = coopland_picture_url , caption = post, parse_mode="Markdown")
+
+
+
+
+
