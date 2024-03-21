c35cd3a763393bdea6bc08b30b5d58dd199cff3b
Elkarapuzik <shmakov-10@bk.ru>
2024-03-21 18:36:47 +0300

    Добавлена беск отправка + блок if main

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from pprint import pprint
+import time
 
 #______________________________
 
@@ -12,7 +13,6 @@ from dotenv import load_dotenv
 load_dotenv()
 #______________________________
 
-coopland_link = 'https://coop-land.ru'
 
 def get_coopland_info(coopland_link_base):
 
@@ -33,25 +33,30 @@ def get_coopland_info(coopland_link_base):
     
     return last_news_headline, last_news_description, last_news_picture_url, last_news_source_link
 
+#________________________________________________________________________________________________________
+if __name__ == "__main__":
 
 
-coopland_headline, coopland_descriotipn, coopland_picture_url, coopland_source_link = get_coopland_info(coopland_link)
+    coopland_link = 'https://coop-land.ru'
 
-post = (f"""\
+    telegram_bot_token = os.getenv("TELEGRAM_BOT_TOKEN")
+    telegram_chat_id = os.getenv("TELEGRAM_CHAT_ID")
+
+    bot = telegram.Bot(token=telegram_bot_token)
+
+    while True:
+        coopland_headline, coopland_descriotipn, coopland_picture_url, coopland_source_link = get_coopland_info(coopland_link)
+
+        post = (f"""\
 {coopland_headline} 
 {coopland_descriotipn}
 [▶▶▶Источник]({coopland_source_link})
 """)
 
 #_______________________________________________________________
-telegram_bot_token = os.getenv("TELEGRAM_BOT_TOKEN")
-telegram_chat_id = os.getenv("TELEGRAM_CHAT_ID")
 
-
-bot = telegram.Bot(token=telegram_bot_token)
-
-bot.send_photo(chat_id=telegram_chat_id, photo = coopland_picture_url , caption = post, parse_mode="Markdown")
-
+        bot.send_photo(chat_id=telegram_chat_id, photo = coopland_picture_url , caption = post, parse_mode="Markdown")
+        time.sleep(300)
 
 
 
